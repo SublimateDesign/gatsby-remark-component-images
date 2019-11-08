5eb6bc7794c3e39873873850bf0301f8af71229f
sublimateDesign <matt.ubben@gmail.com>
2019-11-07 20:17:42 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,162 @@
 # gatsby-remark-component-images
- A Gatsby remark plugin applying gatsby-plugin-sharp processing to html-style markdown tags.
+
+ A Gatsby remark plugin applying gatsby-plugin-sharp processing to html-style markdown tags. The results can then be converted into 
+
+## Install
+
+`npm install --save gatsby-remark-component-images`
+
+OR
+
+`yarn add gatsby-remark-component-images`
+
+## How to use
+
+This plugin expects relative image paths. The following configuration uses [gatsby-remark-relative-source](https://github.com/SublimateDesign/gatsby-remark-relative-source) to transform the image paths set by NetlifyCMS
+
+```js
+// gatsby-config.js
+plugins: [
+  // Add static assets before markdown files
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      path: `${__dirname}/static/uploads`,
+      name: 'uploads',
+    },
+  },
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      path: `${__dirname}/src/pages`,
+      name: 'pages',
+    },
+  },
+  {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      plugins: [
+        // gatsby-remark-relative-source should be added before plugins needing relative sources
+        {
+          resolve: `gatsby-remark-relative-source`,
+          options: {
+            name: `uploads`,
+            htmlSources: [{tagName: `post-video`, attributes: [`image`]}] // post-video is a component referenced later by gatsby-remark-custom-image-component
+          },
+        },
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            // It's important to specify the maxWidth (in pixels) of
+            // the content container as this plugin uses this as the
+            // base for generating different widths of each image.
+            maxWidth: 590,
+          },
+        },
+        {
+          resolve: `gatsby-remark-component-images`,
+          options: {
+            // plugin options
+            components: [
+              // source attributes can be defined in the array as strings or objects
+              // string represenation maps to the source attribute and the target attribute defaults to the source attribute prefixed by the sharpMethod
+              // so below becomes [{source: `image`, target: `fluidimage`}]
+              {tagName: `post-video`, attributes: [`image`]},
+              // object representation can be used to apply/override options or define the target attribute
+              // object and string representation can be mixed to suit requirement
+              {tagName: `gallery-media`, attributes: [{source: `src`, target: `fluidimg`, options: {tracedSVG: {color: "rgb(248, 255, 93)"},`widescreen`]}
+            ],
+            sharpMethod: 'fluid',
+            // fluid's arguments
+            quality: 50,
+            maxWidth: 2500,
+            withWebp: true
+          }
+        },
+      ],
+    },
+  },
+];
+```
+
+Source markdown:
+
+```markdown
+# src/pages/index.md
+---
+title: Hello World
+---
+
+<post-video url="https://vimeo.com/yourvideoid" image="/img/videoimage.png"></post-video>
+
+<media-gallery displayclass="columns">
+    <gallery-media displayclass="column is-one-third" src="/img/image1.png" widescreen="/img/widescreenimage1.png" title="Image 1"></gallery-media>
+    <gallery-media displayclass="column is-one-third" src="/img/image2.png" widescreen="/img/widescreenimage2.png" title="Image 2"></gallery-media>
+    <gallery-media displayclass="column is-one-third" src="/img/image3.png" widescreen="/img/widescreenimage3.png" title="Image 3"></gallery-media>
+</media-gallery>
+
+```
+
+Component definitions in the template using rehype-react:
+
+```js
+//src/components/Content
+import React from 'react'
+import rehypeReact from 'rehype-react'
+import PostVideo from './PostVideo'
+import MediaGallery from './MediaGallery'
+//...
+
+const renderAst = new rehypeReact({
+  createElement: React.createElement,
+  components: { "post-video": PostVideo, "media-gallery": MediaGallery }
+}).Compiler
+
+export const HtmlComponentContent = ({content, className}) => (
+  <div className={className}>{renderAst(content)}</div>
+)
+
+```
+
+Parsing results in the components:
+
+```js
+//src/components/PostVideo
+import React from 'react'
+//...
+
+const PostVideo = ({url, image, fluidimage}) => {
+    const responsiveImage = JSON.parse(fluidimage)
+    //...
+}
+```
+
+```js
+//src/components/MediaGallery
+import React from 'react'
+import Img from 'gatsby-image'
+//...
+
+const MediaGallery = ({children, displayclass}) => {
+    let allMedia = []
+    if (children && children.length) {
+        children.filter(child => child !== '\n' && child.props && child.type === 'gallery-media' && child.props.fluidimg).map(child => {
+            let sources = [JSON.parse(child.props.fluidmobile)]
+            if (child.props.fluidwidescreen) {
+                sources.push({...(JSON.parse(child.propsfluidwidescreen)), media: `(min-width: 1500px)`})
+            }
+            allMedia.push({sources: sources, title: child.props.title, displayclass: child.props.displayclass)
+        })
+    }
+    return (
+        <div className={displayclass}>
+            {allMedia.map(media => 
+                <div className={media.displayclass}>
+                    <Img fluid={media.sources} display></Img>
+                </div>
+            )}            
+        </div>
+    )
+    //...
+}
+```
---
+++ index.js
@@ -0,0 +1,151 @@
+const _ = require('lodash')
+const cheerio = require('cheerio')
+const isRelativeUrl = require('is-relative-url')
+const path = require('path')
+const Promise = require('bluebird')
+const sharp = require('gatsby-plugin-sharp')
+const slash = require('slash')
+const visitWithParents = require('unist-util-visit-parents')
+
+module.exports = ({ files, markdownNode, markdownAST, getNode, reporter, cache, }, pluginOptions) => {
+    const defaults = {
+        components: [{tagName: 'component-image', attributes: [{source: 'src', target: ''}]}],
+        sharpFunction: 'fluid',
+    }
+
+    const options = _.defaults(pluginOptions, defaults)
+
+    const generateImages = async (imageSource, optionsOverride) => {
+        const parentNode = getNode(markdownNode.parent)
+        if (!(parentNode && parentNode.dir)) {
+            return null
+        }
+        
+        const imagePath = slash(path.join(parentNode.dir, imageSource))
+        const imageNode = _.find(files, (file) => {
+            if (file && file.absolutePath) {
+            return file.absolutePath === imagePath
+            }
+            return null
+        })
+
+        if (!imageNode || !imageNode.absolutePath) {
+            return null
+        }
+
+        const applyOptions = optionsOverride !== null && (typeof optionsOverride === `object`) ? _.defaults(optionsOverride, options) : options
+
+        const result = await sharp[applyOptions.sharpFunction]({
+            file: imageNode,
+            args: applyOptions,
+            reporter,
+            cache,
+        })
+
+        if (applyOptions.withWebp) {
+            const webpResult = await sharp[applyOptions.sharpFunction]({
+                file: imageNode,
+                args: _.defaults({toFormat: `WEBP`}, applyOptions),
+                reporter,
+                cache,
+            })
+            if (webpResult) {
+                result.srcWebp = webpResult.src
+                result.srcSetWebp = webpResult.srcSet
+            }
+        }
+
+        if (applyOptions.tracedSVG) {
+            let args = typeof applyOptions.tracedSVG === `object` ? applyOptions.tracedSVG : {}
+
+            // Translate Potrace constants (e.g. TURNPOLICY_LEFT, COLOR_AUTO) to the values Potrace expects
+            const { Potrace } = require(`potrace`)
+            const argsKeys = Object.keys(args)
+            args = argsKeys.reduce((result, key) => {
+                const value = args[key]
+                result[key] = Potrace.hasOwnProperty(value) ? Potrace[value] : value
+                return result
+            }, {})
+
+            const tracedSVG = await sharp['traceSVG']({
+                file: imageNode,
+                args,
+                fileArgs: args,
+                cache,
+                reporter,
+            })
+
+            // Escape single quotes so the SVG data can be used in inline style attribute with single quotes
+            result.tracedSVG = tracedSVG.replace(/'/g, `\\'`)
+            if (!applyOptions.preserveBase64) {
+                // Components such as Img (gatsby-img) use blurUp when base64 is provided. Assume the traced SVG is the preferred placeholder image when requested
+                result.base64 = null
+            }
+        }
+
+        return result
+    }
+
+    const rawHtmlNodes = []
+    visitWithParents(markdownAST, 'html', (node) => { rawHtmlNodes.push(node) })
+
+    return Promise.all(
+        rawHtmlNodes.map(node => 
+            new Promise(async (resolve) => {
+                if (!node.value) {
+                    return resolve(node)
+                }
+                const $ = cheerio.load(node.value)
+                let processComponents = []
+                // add any elements matching tagName set in the check node definitions
+                for (const componentDefinition of options.components) {
+                    $(componentDefinition.tagName).each(function() {
+                        processComponents.push({ ref: $(this), attributes: componentDefinition.attributes})
+                    })
+                }
+                if (!processComponents.length) {
+                    // No matching elements
+                    return resolve(node)
+                }
+
+                for (const component of processComponents) {
+                    for (const attribute of component.attributes) {                    
+                        let source = attribute
+                        let optionsOverride = null
+                        let target = ''
+                        if (typeof attribute === `object`) {
+                            source = attribute.source
+                            optionsOverride = attribute.options || null
+                            if (attribute.target) {
+                                target = attribute.target
+                            }
+                            else if (optionsOverride && optionsOverride.sharpFunction) {
+                                target = optionsOverride.sharpFunction + source
+                            }
+                        }
+                        if (!target) {
+                            target = options.sharpFunction + source
+                        }    
+                        const imageSource = component.ref.attr(source)
+                        if (!imageSource) {
+                            continue
+                        }           
+                        const fileType = imageSource.split(".").reverse()[0]
+                        // sharp can't process gifs or svgs
+                        if (fileType !== 'gif' && fileType !== 'svg' && isRelativeUrl(imageSource)) {
+                            const result = await generateImages(imageSource, optionsOverride)
+                            if (result) {
+                                // set result to the targeted attribute
+                                component.ref.attr(target, JSON.stringify(result))
+                            }
+                        }
+                    }
+                }
+
+                node.type = 'html'
+                node.value = $('body').html()
+                return resolve(node)
+            })
+        )
+    )
+}
